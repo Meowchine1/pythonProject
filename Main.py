65225bba509f92ec50c9ba2235009818c22cac84
Meowchine1 <katevoronina128@gmail.com>
2023-03-28 20:26:16 +0300

    task is done

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 # формирует интервал времени о τ, по истечении которого завершится обслуживание требования
 import math
 import random
-
 import Condition
 from Constant import *
 from Device import Device
@@ -19,46 +18,49 @@ def next_moment(n, coef):
 
 
 def requirements_segment(t):
-    global N_requirement, device
-    requirement = Requirement()
-    if device.get_Q_size() < queue_max_size:
-        requirement.set_t_postyp(t)
-        device.put_into_Q(requirement)
+    global N_requirement, device, fail_req
+    requirement = Requirement() # создает новое требовани
+    if device.get_Q_size() < queue_max_size:  # проверка наличия места в очереди
+        requirement.set_t_postyp(t) # наделяет это требование значениями атрибутов
+        device.put_into_Q(requirement) # ставит требование в очередь
         N_requirement += 1
-        # формирует интерфал времени (длительность интервала времени между очередными поступлениями требований)
-        # по истечении которого повторится работа этого сегмента
-        # определяется очередной момент выполнения сегмента
-        device.set_s_postyp(next_moment(t, lambd))
-        device.set_s_start(next_moment(t, nu)) # nu
-    # сегмент завершшает работу
+        #device.set_s_start(next_moment(t, nu)) # nu
+    # иначе отказ в обслуживании
+    device.set_s_postyp(next_moment(t, lambd)) # определяет очередной момент выполнения сегмента процесса
+    fail_req += 1
 
+
+def set_start_segment(t):
+    if not device.is_Q_Empty():
+        device.set_s_start(next_moment(t, nu))  # nu
+    else:
+        device.set_s_start(infinity)
 
 def start_service_segment(t):
     global device
-    if device.is_Q_Empty():
-        device.set_s_start(infinity)
-        # завершение работы сегмента
-    elif device.get_condition() != Condition.Condition.BUSY:
+    if device.is_Q_Empty():  # Если очередь Q пуста
+        device.set_s_start(infinity) # сегмент завершает свою работу
+    elif device.get_condition() != Condition.Condition.BUSY:  # если устройство не занято
         requirement = device.take_Q_elem()  # удалили из очереди задачу
-        # состояние занят
-        device.set_condition(Condition.Condition.BUSY)
-        requirement.set_t_start(t)
-        device.set_s_end(next_moment(t, nu))
-        device.set_s_start(infinity)
-        device.set_actual_requirement(requirement)
+        device.set_condition(Condition.Condition.BUSY) # переводит прибор в состояние «занят»
+        requirement.set_t_start(t) # наделяет выбранное требование значениями атрибутов
+
+        device.set_s_end(next_moment(t, nu)) # момент выполнения сегмента процесса, связанного с уходом требования
+        device.set_s_start(infinity)  # момент активизации сегмента начала устанавливается в бесконечность
+        device.set_actual_requirement(requirement) # требование над которым будет работать сегмент процесса
     else:
-        device.set_s_start(next_moment(t, nu))
+        device.set_s_start(infinity) # если устройство занято, то завершаем процесс. Ждем момента от сегмента завершения
 
 def end_service_segment(t):
     global N_requirement, device
-    # export from queue
+
     requirement = device.get_actual_requirement()
-    requirement.set_t_end(t)
-    device.put_into_Q_served(requirement)
+    requirement.set_t_end(t) # изменяет значения атрибутов требования
+    device.put_into_Q_served(requirement) # ставит требование, завершившее обслуживание, в очередь
     N_requirement -= 1
-    device.set_condition(Condition.Condition.FREE)
-    device.set_s_start(next_moment(t, nu))
-    device.set_s_end(infinity)
+    device.set_condition(Condition.Condition.FREE) # переводит прибор в состояние «свободен»
+    device.set_s_start(t) #активизируется сегмента процесса начала обслуживания
+    device.set_s_end(infinity) # сегмент ухода завершает работу
     return requirement.get_t_end() - requirement.get_t_start() + requirement.get_t_start() - requirement.get_t_postyp()
 
 
@@ -66,6 +68,7 @@ def end_service_segment(t):
 n_n = 0
 device = Device()
 N_requirement = 0
+fail_req = 0
 
 interval_0_start = infinity
 interval_0_end = infinity
@@ -136,13 +139,13 @@ def time_fixation(i):
 
     elif N_requirement == 3:
         interval_2_start, interval_2_end, interval_2, interval_4_start, interval_4_end, interval_4, \
-        interval_3 = redirect_timer(interval_2_start, interval_2_end, interval_2, interval_4_start,
-                                    interval_4_end, interval_4, interval_3, i)
+        interval_3_start = redirect_timer(interval_2_start, interval_2_end, interval_2, interval_4_start,
+                                    interval_4_end, interval_4, interval_3_start, i)
 
     elif N_requirement == 4:
         interval_3_start, interval_3_end, interval_3, interval_5_start, interval_5_end, interval_5, \
-        interval_4 = redirect_timer(interval_3_start, interval_3_end, interval_3, interval_5_start,
-                                    interval_5_end, interval_5, interval_4, i)
+        interval_4_start = redirect_timer(interval_3_start, interval_3_end, interval_3, interval_5_start,
+                                    interval_5_end, interval_5, interval_4_start, i)
 
     elif N_requirement == 5:
         if interval_5_start == infinity:
@@ -157,7 +160,6 @@ def time_fixation(i):
 while device.get_Q_served_size() < N:
     i = min(device.get_s_postyp(), device.get_s_start(), device.get_s_end())
     time_fixation(i)
-
     if device.get_s_postyp() == i:
         requirements_segment(i)
 
@@ -167,5 +169,34 @@ while device.get_Q_served_size() < N:
     if device.get_s_end() == i:
         n_n += end_service_segment(i)
 
+    if (device.get_s_start() < i) | (device.get_s_start() == infinity):  # лишние установки времени
+        set_start_segment(i)
+
+######################################################################################################
+
 K = device.get_Q_served_size()
-print(interval_1)
+T = i
+mat_exp = 1/T
+q = device.get_Q_served()
+sum_ = 0
+while not q.empty():
+  end = q.get().get_t_end()
+  queue_moment = q.get().get_t_start()
+  sum_ += (end - queue_moment)
+
+print("оценка математического ожидания длительности пребывания требований в системе обслуживания =", sum_ / K)
+
+
+interval1_sum = sum(interval_1) / T
+interval2_sum = sum(interval_2) / T
+interval3_sum = sum(interval_3) / T
+interval4_sum = sum(interval_4) / T
+interval5_sum = sum(interval_5) / T
+
+n_ = interval1_sum + 2 * interval2_sum + 3 * interval3_sum + 4 * interval4_sum + 5 * interval5_sum
+
+print("оценка математического ожидания числа требований в системе обслуживания =", n_)
+
+
+
+print("вероятность отказа в обслуживании требования =", (fail_req / K)  )
